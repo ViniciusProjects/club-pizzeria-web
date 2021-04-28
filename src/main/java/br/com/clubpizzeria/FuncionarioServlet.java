368e4ee0e07e6a9e2dfbf3938f52e0840484a690
vinicius <Vinisilva2001@hotmail.com>
2021-04-27 22:33:07 -0300

    Adicionando Funcionarios ao projeto

--- src/main/java/br/com/clubpizzeria/FuncionarioServlet.java
+++ src/main/java/br/com/clubpizzeria/FuncionarioServlet.java
@@ -1,40 +1,46 @@
 package br.com.clubpizzeria;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FuncionarioServlet
- */
+import com.google.gson.Gson;
+
+import br.com.clubpizzeria.model.Funcionario;
+
 @WebServlet(value = "/funcionario")
 public class FuncionarioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FuncionarioServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().write("pagina de funcionario");
+	public FuncionarioServlet() {
+		super();
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Funcionario funcionarioVinicius = new Funcionario();
+		funcionarioVinicius.setNome("Vinicius");
+		List<Funcionario> funcionarios = new ArrayList<>();
+		funcionarios.add(funcionarioVinicius);
+
+		Funcionario funcionarioVitor = new Funcionario();
+		funcionarioVitor.setNome("Vitor");
+		funcionarios.add(funcionarioVitor);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(funcionarios);
+		response.getWriter().write(json);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
---
+++ src/main/java/br/com/clubpizzeria/model/Funcionario.java
@@ -0,0 +1,14 @@
+package br.com.clubpizzeria.model;
+
+public class Funcionario {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
