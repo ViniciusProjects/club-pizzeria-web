2f85a57ce42ff139227a509681436f492517ec99
vinicius <Vinisilva2001@hotmail.com>
2021-04-27 21:46:32 -0300

    PizzasServlet e falta testar FuncionarioServlet

--- src/main/java/br/com/clubpizzeria/PizzasServlet.java
+++ src/main/java/br/com/clubpizzeria/PizzasServlet.java
@@ -7,18 +7,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import br.com.clubpizzeria.model.Pizza;
+
 @WebServlet(value = "/pizzas")
 public class PizzasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public PizzasServlet() {
+
 		super();
 
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().write("Vc está na pagina de pizzas");
+		Pizza pizza = new Pizza();
+		pizza.setDescricao("Pizza de quatro de queijos");
+		Gson gson = new Gson();
+		gson.toJson(pizza);
+		String json = gson.toJson(pizza);
+		response.getWriter().write(json);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
