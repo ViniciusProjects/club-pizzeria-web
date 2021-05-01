2309bac9891e043cd3ccbfb7f087068f4a76790a
vinicius <Vinisilva2001@hotmail.com>
2021-04-30 21:34:16 -0300

    Add all files this branch

--- src/main/java/br/com/clubpizzeria/UsuarioLogadoServlet.java
+++ src/main/java/br/com/clubpizzeria/UsuarioLogadoServlet.java
@@ -10,29 +10,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value = "/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet(value = "/usuario")
+public class UsuarioLogadoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public LoginServlet() {
+	public UsuarioLogadoServlet() {
 		super();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.getWriter().append("Tela de login").append(request.getContextPath());
+		response.getWriter().append("Tela de login do usuário logado").append(request.getContextPath());
 
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.jsp");
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("usuariologado.jsp");
 		requestDispatcher.forward(request, response);
-		
+
 		String usuario = request.getParameter("Usuário");
-		PrintWriter out = response.getWriter();
-		out.println(usuario); //jsp aqui, acho
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+
+		writer.println("<");
+		writer.println("<li>nome: " + usuario + "</li>");
+
+		System.out.println("Usuario logado foi chamado");
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.sendRedirect("/usuario");
 
 		doGet(request, response);
 	}
